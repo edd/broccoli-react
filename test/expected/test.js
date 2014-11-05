71921d5cc9aa6765fc361b5c7948d695ee2bbbe4
Ludovico Fischer <livrerie@gmail.com>
2014-11-05 08:51:53 +0100

    Update React dependency to 0.12.x.

--- test/expected/test.js
+++ test/expected/test.js
@@ -1,11 +1,10 @@
-/** @jsx React.DOM */
 /*jslint indent: 2, node: true, nomen: true, browser: true*/
 /*global React */
 'use strict';
 module.exports = React.createClass({displayName: 'exports',
   render: function () {
     return (
-      React.DOM.h1(null, "Output")
+      React.createElement("h1", null, "Output")
     );
   }
 });
